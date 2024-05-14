79dd80486bd5b8229e9a00d44dbbb4be8f13c62e
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-05-14 19:40:17 -0300

    adicionando teste serializer matricula

--- escola/tests/test_serializers.py
+++ escola/tests/test_serializers.py
@@ -27,7 +27,7 @@ class SerializerEstudanteTestCase(TestCase):
         self.assertEqual(dados['data_nascimento'],self.estudante.data_nascimento)
         self.assertEqual(dados['celular'],self.estudante.celular)
 
-class SerializerEstudanteTestCase(TestCase):
+class SerializerCursoTestCase(TestCase):
     def setUp(self):
         self.curso = Curso(
             codigo = 'CTM',
@@ -47,3 +47,34 @@ class SerializerEstudanteTestCase(TestCase):
         self.assertEqual(dados['codigo'],self.curso.codigo)
         self.assertEqual(dados['descricao'],self.curso.descricao)
         self.assertEqual(dados['nivel'],self.curso.nivel)
+
+class SerializerMatriculaTestCase(TestCase):
+    def setUp(self):
+        self.estudante_matricula = Estudante.objects.create(
+            nome = 'Teste Modelo Matricula',
+            email='testemodelomatricula@gmail.com',
+            cpf='91546870040',
+            data_nascimento='2003-02-02',
+            celular='86 99999-9999'
+        )
+        self.curso_matricula = Curso.objects.create(
+            codigo='CTMM',descricao='Curso Teste Modelo Matricula',nivel='B'
+        )
+        self.matricula = Matricula.objects.create(
+            estudante=self.estudante_matricula,
+            curso=self.curso_matricula,
+            periodo='M'
+        )
+        self.serializer_matricula = MatriculaSerializer(instance=self.matricula)
+
+    def test_verifica_campos_serializados_de_matricula(self):
+        """Teste que verifica os campos que estão sendo serializados de matricula"""
+        dados = self.serializer_matricula.data
+        self.assertEqual(set(dados.keys()),set(['id','estudante','curso','periodo']))  
+    
+    def test_verifica_conteudo_dos_campos_serializados_de_estudante(self):
+        """Teste que verifica o conteúdo dos campos que estão sendo serializados de estudante"""
+        dados = self.serializer_matricula.data
+        self.assertEqual(dados['estudante'],self.matricula.estudante.id)
+        self.assertEqual(dados['curso'],self.matricula.curso.id)
+        self.assertEqual(dados['periodo'],self.matricula.periodo)
