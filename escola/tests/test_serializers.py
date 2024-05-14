f58dd5a693a396dd0755ce83eeec064340e55336
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-05-14 19:35:18 -0300

    adicionando teste serializer curso

--- escola/tests/test_serializers.py
+++ escola/tests/test_serializers.py
@@ -26,3 +26,24 @@ class SerializerEstudanteTestCase(TestCase):
         self.assertEqual(dados['cpf'],self.estudante.cpf)
         self.assertEqual(dados['data_nascimento'],self.estudante.data_nascimento)
         self.assertEqual(dados['celular'],self.estudante.celular)
+
+class SerializerEstudanteTestCase(TestCase):
+    def setUp(self):
+        self.curso = Curso(
+            codigo = 'CTM',
+            descricao = 'Curso Teste Modelo',
+            nivel = 'B',
+        )
+        self.serializer_curso = CursoSerializer(instance=self.curso)
+
+    def test_verifica_campos_serializados_de_curso(self):
+        """Teste que verifica os campos que estão sendo serializados de curso"""
+        dados = self.serializer_curso.data
+        self.assertEqual(set(dados.keys()),set(['id','codigo','descricao','nivel']))  
+    
+    def test_verifica_conteudo_dos_campos_serializados_de_estudante(self):
+        """Teste que verifica o conteúdo dos campos que estão sendo serializados de estudante"""
+        dados = self.serializer_curso.data
+        self.assertEqual(dados['codigo'],self.curso.codigo)
+        self.assertEqual(dados['descricao'],self.curso.descricao)
+        self.assertEqual(dados['nivel'],self.curso.nivel)
