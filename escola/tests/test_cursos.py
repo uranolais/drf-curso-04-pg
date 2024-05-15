83c7ffda541a8a794564b58b124f18795eea4378
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-05-15 13:54:19 -0300

    adicionando GET e congiguração

--- escola/tests/test_cursos.py
+++ escola/tests/test_cursos.py
@@ -2,8 +2,20 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from escola.models import Curso
 
 class CursosTestCase(APITestCase):
     def setUp(self):
+        self.url = reverse('Cursos-list')
         self.usuario = User.objects.create_superuser(username='lais',password='lais')
         self.client.force_authenticate(user=self.usuario)
+        self.curso_01 = Curso.objects.create(
+            codigo='CT01',descricao='Curso Teste 01',nivel='B'
+        )
+        self.curso_02 = Curso.objects.create(
+            codigo='CT02',descricao='Curso Teste 02',nivel='I'
+        )
+    def test_requisicao_get_para_listar_cursos(self):
+        """Teste para verificar a requisição GET para listar os cursos"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- escola/tests/test_estudantes.py
+++ escola/tests/test_estudantes.py
@@ -2,9 +2,31 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from escola.models import Estudante
 
 class EstudanteTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin',password='admin')
         self.client.force_authenticate(user=self.usuario)
+        self.url = reverse("Estudantes-list")
+
+        self.estudante_01 = Estudante.objects.create(
+            nome = 'Teste de Estudante UM',
+            email = 'testeestudante01@gmail.com',
+            cpf = '27151855028',
+            data_nascimento = '2023-02-02',
+            celular = '86 99999-9999',
+        )
+        self.estudante_02 = Estudante.objects.create(
+            nome = 'Teste de Estudante DOIS',
+            email = 'testeestudante02@gmail.com',
+            cpf = '99326390012',
+            data_nascimento = '2023-02-02',
+            celular = '86 99999-9999',
+        )
+
+    def test_requisicao_get_para_listar_estudantes(self):
+        """Teste para verificar a requisição GET para listar os estudantes"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         
